f97746d65579eb9fea873d5110290c5a3605d617
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-16 18:12:20 +0300

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExit=false;
+
+            while (isExit==false)
+            {
+                string day;
+                Console.Write("Напишите любой день недели или exit чтобы завершить: ");
+                day = Console.ReadLine();
+
+                switch (day)
+                {
+                    case "понедельник":
+                    case "вторник":
+                    case "среда":
+                    case "четверг":
+                    case "пятница":
+                    case "суббота":
+                    case "воскресенье":
+                        Console.WriteLine("день недели");
+                        break;
+                    case "exit":
+                        Console.WriteLine("Выход из программы");
+                        isExit = true;
+                        break; 
+                    default:
+                        Console.WriteLine("Я такого дня недели не знаю");
+                        break;
+                }
+            }
+        }
+        }
+    }
+    
+
